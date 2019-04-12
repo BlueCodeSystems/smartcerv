edd5ad0fb16d4f5b9be4afea698270aa4635d02a
Mellorison <mellorison@gmail.com>
2019-04-12 09:00:27 +0200

    Added Gallery Access Module and digital cervicography functions

---
+++ cervicalcancer/src/main/java/zm/gov/moh/cervicalcancer/submodule/dashboard/patient/view/PatientImageMapComparator.java
@@ -0,0 +1,34 @@
+package zm.gov.moh.cervicalcancer.submodule.dashboard.patient.view;
+
+
+
+import java.util.Comparator;
+import java.util.HashMap;
+
+
+class MapComparator implements Comparator<HashMap<String, String>>
+{
+    private final String key;
+    private final String order;
+
+    public MapComparator(String key, String order)
+    {
+        this.key = key;
+        this.order = order;
+    }
+
+    public int compare(HashMap<String, String> first,
+                       HashMap<String, String> second)
+    {
+        // TODO: Null checking, both for maps and values
+        String firstValue = first.get(key);
+        String secondValue = second.get(key);
+        if(this.order.toLowerCase().contentEquals("asc"))
+        {
+            return firstValue.compareTo(secondValue);
+        }else{
+            return secondValue.compareTo(firstValue);
+        }
+
+    }
+}
