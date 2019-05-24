4f90d58a890ae590a78a830fa3c361b760c6b134
Kaunda Kabwe - Work PC <kkabwe@bluecodeltd.com>
2019-05-24 12:14:06 +0200

    progress on issue#15 - BasicDrug widget rendering. working on making the bundle before submitting.

--- common/src/main/java/zm/gov/moh/common/submodule/form/widget/BasicDrugWidget.java
+++ common/src/main/java/zm/gov/moh/common/submodule/form/widget/BasicDrugWidget.java
@@ -1,45 +1,52 @@
 package zm.gov.moh.common.submodule.form.widget;
 
 import android.content.Context;
-import android.os.Bundle;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatSpinner;
-import androidx.appcompat.widget.AppCompatTextView;
-import androidx.appcompat.widget.LinearLayoutCompat;
-import androidx.lifecycle.LiveData;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-import zm.gov.moh.core.repository.database.dao.derived.ConceptAnswerNameDao;
+import zm.gov.moh.common.R;
+import zm.gov.moh.common.submodule.form.model.Form;
+import zm.gov.moh.common.submodule.form.model.Logic;
+import zm.gov.moh.core.model.DrugObsValue;
 import zm.gov.moh.core.repository.database.entity.derived.ConceptAnswerName;
 import zm.gov.moh.core.repository.database.entity.domain.Drug;
 
 public class BasicDrugWidget extends RepositoryWidget<String> {
 
-    protected String mValue;
     protected String mUuid;
-    protected Drug drugObject;
-    protected CheckBox mCheckbox;
-    protected AppCompatTextView mTextView;
     protected AppCompatSpinner frequencySpinner;
     protected AppCompatSpinner durationSpinner;
-    Set<Long> answerConcepts;
-    protected Map<String, Long> frequencyIdMap = new HashMap<>();;
-    protected Map<String, Long> durationIdMap = new HashMap<>();;
-    protected Map<String, Long> conceptNameIdMap = new HashMap<>();
-    protected Set<Long> answerConceptIds;
 
+    protected TableLayout tableLayout;
+    protected TableRow tableRow;
+    protected TableRow.LayoutParams rowLayoutParams;
+    protected TableLayout.LayoutParams layoutParams;
+
+    protected Map<String, Long> frequencyIdMap = new HashMap<>();
+    protected Map<String, Long> durationIdMap = new HashMap<>();
+    protected Map<String, Long> checkboxNameIdMap = new HashMap<>();
+    LinkedHashSet<Long> answerConcepts;
+
+    AtomicBoolean canSetValue;
+    Object mValue;
+    DrugObsValue<Object> mDrugObsValue;
+    List<Logic> logic;
+    Form form;
 
     private BasicDrugWidget(Context context) {
         super(context);
@@ -67,15 +74,24 @@ public class BasicDrugWidget extends RepositoryWidget<String> {
 
     public void onCreateView(){
 
+        //WidgetUtils.setLayoutParams(this,WidgetUtils.MATCH_PARENT,WidgetUtils.WRAP_CONTENT);
+        layoutParams = new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT,
+            TableLayout.LayoutParams.WRAP_CONTENT);
+
+        tableLayout = new TableLayout(mContext);
+        tableLayout.setBackground(mContext.getResources().getDrawable(R.drawable.border_bottom));
+        tableLayout.setLayoutParams(layoutParams);
+
+        rowLayoutParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT,
+            TableRow.LayoutParams.WRAP_CONTENT);
+
         //frequency list
         mRepository.getDatabase()
             .conceptAnswerNameDao()
             .getByConceptId(165225L)
             .observe((AppCompatActivity)mContext, this::onFrequencyAnswersNamesReceived);
 
-
-
-
+        addView(tableLayout);
     }
 
     public void onFrequencyAnswersNamesReceived(List<ConceptAnswerName> frequencyAnswerNames) {
@@ -85,9 +101,9 @@ public class BasicDrugWidget extends RepositoryWidget<String> {
 
         //duration lists
         mRepository.getDatabase()
-                .conceptAnswerNameDao()
-                .getByConceptId(165224L)
-                .observe((AppCompatActivity)mContext, this::onDurationAnswersNamesReceived);
+            .conceptAnswerNameDao()
+            .getByConceptId(165224L)
+            .observe((AppCompatActivity)mContext, this::onDurationAnswersNamesReceived);
     }
 
     public void onDurationAnswersNamesReceived(List<ConceptAnswerName> durationAnswerNames) {
@@ -95,51 +111,93 @@ public class BasicDrugWidget extends RepositoryWidget<String> {
         for(ConceptAnswerName conceptAnswerName: durationAnswerNames)
             durationIdMap.put(conceptAnswerName.name, conceptAnswerName.answer_concept);
 
-        mRepository.getDatabase().drugDao().getDrugNameByUuid(mUuid)
+        mRepository.getDatabase()
+                .drugDao()
+                .getDrugNameByUuid(mUuid)
                 .observe((AppCompatActivity)mContext, this::onDrugReceived);
     }
 
     public void onDrugReceived(Drug drug) {
 
-        int orientation = (conceptNameIdMap.size() > 2)? WidgetUtils.VERTICAL: WidgetUtils.HORIZONTAL;
-        mCheckbox = new CheckBox(mContext);
-        RadioGroup checkBoxGroup = WidgetUtils.createCheckBoxes(mContext, conceptNameIdMap,this::onCheckedChanged, orientation,WidgetUtils.WRAP_CONTENT,WidgetUtils.WRAP_CONTENT,1);
+        answerConcepts = new LinkedHashSet<>();
+
+        String strength = "";
+        if(drug.strength != null)
+            strength = drug.strength;
+        checkboxNameIdMap.put(drug.name + " " + strength, drug.concept_id);
+
+        int orientation = (checkboxNameIdMap.size() > 2)? WidgetUtils.VERTICAL: WidgetUtils.HORIZONTAL;
+
+        RadioGroup checkBoxGroup = WidgetUtils.createCheckBoxes(mContext, checkboxNameIdMap,this::onCheckedChanged, orientation,WidgetUtils.WRAP_CONTENT,WidgetUtils.WRAP_CONTENT,1);
+        tableRow = new TableRow(mContext);
+        tableRow.setBackground(mContext.getResources().getDrawable(R.drawable.border_bottom));
 
         frequencySpinner = WidgetUtils.createSpinner(mContext, frequencyIdMap, this::onSelectedValue,
             WidgetUtils.WRAP_CONTENT, WidgetUtils.WRAP_CONTENT, 1);
-
         durationSpinner = WidgetUtils.createSpinner(mContext, durationIdMap, this::onSelectedValue,
             WidgetUtils.WRAP_CONTENT, WidgetUtils.WRAP_CONTENT, 1);
 
-        LinearLayoutCompat.LayoutParams layoutParams =  new LinearLayoutCompat.LayoutParams(
-            LinearLayoutCompat.LayoutParams.WRAP_CONTENT, LinearLayoutCompat.LayoutParams.WRAP_CONTENT);
-        layoutParams.weight = 1;
+        checkBoxGroup.setLayoutParams(rowLayoutParams);
+        frequencySpinner.setLayoutParams(rowLayoutParams);
+        durationSpinner.setLayoutParams(rowLayoutParams);
 
-        //mTextView.setLayoutParams(layoutParams);
-        frequencySpinner.setLayoutParams(layoutParams);
-        durationSpinner.setLayoutParams(layoutParams);
+        tableRow.addView(checkBoxGroup);
+        tableRow.addView(frequencySpinner);
+        tableRow.addView(durationSpinner);
 
-        mTextView = new AppCompatTextView(mContext);
-        mTextView.setText(drug.name);// retrieve name using uuid here
-        mTextView.setTextSize(18);
+        tableLayout.addView(tableRow);
+    }
 
-        addView(mCheckbox);
-        addView(mTextView);
-        addView(frequencySpinner);
-        addView(durationSpinner);
+    private void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+        long id = (long)compoundButton.getId();
 
+        if(!(checked) && answerConcepts.contains(id))
+            answerConcepts.remove(id);
+        else
+            answerConcepts.add(id);
+
+        setObsValue(answerConcepts);
+    }
+
+    public void setObsValue(Object obsValue) {
+
+        if(canSetValue.get()) {
+            mValue = obsValue;
+            mDrugObsValue.setValue(obsValue);
+
+            /*for (Logic logic : logic) {
+                if (logic.getAction().getType().equals("skipLogic"))
+                    if ((mValue != null) && (((Set<Long>) mValue).contains(Math.round((Double) logic.getCondition().getValue())))) {
+                        WidgetUtils.applyOnViewGroupChildren(form.getRootView(), v -> v.setVisibility(VISIBLE), logic.getAction().getMetadata().getTags().toArray());
+                        form.getFormContext().getVisibleWidgetTags().removeAll(logic.getAction().getMetadata().getTags());
+                    } else {
+
+                        Set<String> tags = new HashSet<>();
+                        WidgetUtils.extractTagsRecursive(form.getRootView(), tags, logic.getAction().getMetadata().getTags());
+                        form.getFormContext().getVisibleWidgetTags().addAll(tags);
+                    }
+            }*/
+            //render();
+        }
     }
 
     private void onSelectedValue(Long value) {
         /*if(!answerConcepts.isEmpty())
             answerConcepts.clear();
 
-        answerConcepts.add(value);*/
-        //setObsValue(answerConcepts);
+        answerConcepts.add(value);
+        //setObsValue(answerConcepts);*/
     }
 
-    private void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+    public void render(){
 
+        WidgetUtils.applyOnViewGroupChildren(form.getRootView(),
+                v ->{
+                    v.setVisibility(GONE);
+                    if(v instanceof BasicDrugWidget)
+                        ((BasicDrugWidget)v).reset();
+                },
+                form.getFormContext().getVisibleWidgetTags().toArray());
     }
 
     public static class Builder extends RepositoryWidget.Builder {
@@ -162,6 +220,9 @@ public class BasicDrugWidget extends RepositoryWidget<String> {
 
         public BasicDrugWidget build() {
             BasicDrugWidget widget = new BasicDrugWidget(mContext);
+            widget.canSetValue = new AtomicBoolean();
+            widget.mDrugObsValue = new DrugObsValue<>();
+            widget.canSetValue.set(true);
 
             if(mUuid != null)
                 widget.setUuid(mUuid);
@@ -172,5 +233,29 @@ public class BasicDrugWidget extends RepositoryWidget<String> {
 
             return  widget;
         }
+    }
+
+    public void reset(){
+
+        /*canSetValue.compareAndSet(true,false);
+        if(mStyle != null)
+            switch (mStyle){
+
+                case "check":
+                    WidgetUtils.applyOnViewGroupChildren(this, view -> {
+                        if(view instanceof CheckBox)
+                            ((CheckBox)view).setChecked(false);
+                    });
+                    break;
+
+                case "radio":
+                    WidgetUtils.applyOnViewGroupChildren(this, view -> {
+                        if(view instanceof RadioButton)
+                            ((RadioButton)view).setChecked(false);
+                    });
+                    break;
+            }
+
+        canSetValue.compareAndSet(false,true);*/
     }
 }
