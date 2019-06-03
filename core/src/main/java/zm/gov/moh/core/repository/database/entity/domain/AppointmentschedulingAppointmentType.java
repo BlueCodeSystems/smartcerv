e67314bfab6676f6bc960ab2e669a5a7e1aa0a6b
Kaunda Kabwe - Work PC <kkabwe@bluecodeltd.com>
2019-06-03 15:03:56 +0200

    appointment entities refactored.

--- core/src/main/java/zm/gov/moh/core/repository/database/entity/domain/AppointmentschedulingAppointmentType.java
+++ core/src/main/java/zm/gov/moh/core/repository/database/entity/domain/AppointmentschedulingAppointmentType.java
@@ -2,24 +2,179 @@ package zm.gov.moh.core.repository.database.entity.domain;
 
 import androidx.room.*;
 
+import com.squareup.moshi.Json;
+
 import org.threeten.bp.LocalDateTime;
 
 @Entity(tableName = "appointmentscheduling_appointment_type")
 public class AppointmentschedulingAppointmentType {
 
     @PrimaryKey
-    public Long appointment_type_id;
-    public String name;
-    public String description;
-    public Integer duration;
-    public String uuid;
-    public Integer creator;
-    public LocalDateTime date_created;
-    public Integer changed_by;
-    public LocalDateTime date_changed;
-    public Integer retired;
-    public Integer retired_by;
-    public LocalDateTime date_retired;
-    public String retired_reason;
-    public Integer confidential;
+    @ColumnInfo(name = "appointment_type_id")
+    @Json(name = "appointment_type_id")
+    private Long appointmentTypeId;
+
+    @ColumnInfo(name = "name")
+    @Json(name = "name")
+    private String name;
+
+    @ColumnInfo(name = "description")
+    @Json(name = "description")
+    private String description;
+
+    @ColumnInfo(name = "duration")
+    @Json(name = "duration")
+    private Integer duration;
+
+    @ColumnInfo(name = "uuid")
+    @Json(name = "uuid")
+    private String uuid;
+
+    @ColumnInfo(name = "creator")
+    @Json(name = "creator")
+    private Integer creator;
+
+    @ColumnInfo(name = "date_created")
+    @Json(name = "date_created")
+    private LocalDateTime dateCreated;
+
+    @ColumnInfo(name = "changed_by")
+    @Json(name = "changed_by")
+    private Integer changedBy;
+
+    @ColumnInfo(name = "date_changed")
+    @Json(name = "date_changed")
+    private LocalDateTime dateChanged;
+
+    @ColumnInfo(name = "retired")
+    @Json(name = "retired")
+    private Integer retired;
+
+    @ColumnInfo(name = "retired_by")
+    @Json(name = "retired_by")
+    private Integer retiredBy;
+
+    @ColumnInfo(name = "date_retired")
+    @Json(name = "date_retired")
+    private LocalDateTime dateRetired;
+
+    @ColumnInfo(name = "retired_reason")
+    @Json(name = "retired_reason")
+    private String retiredReason;
+
+    @ColumnInfo(name = "confidential")
+    @Json(name = "confidential")
+    private Integer confidential;
+
+    public Long getAppointmentTypeId() {
+        return appointmentTypeId;
+    }
+
+    public void setAppointmentTypeId(Long appointmentTypeId) {
+        this.appointmentTypeId = appointmentTypeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public Integer getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Integer creator) {
+        this.creator = creator;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Integer getChangedBy() {
+        return changedBy;
+    }
+
+    public void setChangedBy(Integer changedBy) {
+        this.changedBy = changedBy;
+    }
+
+    public LocalDateTime getDateChanged() {
+        return dateChanged;
+    }
+
+    public void setDateChanged(LocalDateTime dateChanged) {
+        this.dateChanged = dateChanged;
+    }
+
+    public Integer getRetired() {
+        return retired;
+    }
+
+    public void setRetired(Integer retired) {
+        this.retired = retired;
+    }
+
+    public Integer getRetiredBy() {
+        return retiredBy;
+    }
+
+    public void setRetiredBy(Integer retiredBy) {
+        this.retiredBy = retiredBy;
+    }
+
+    public LocalDateTime getDateRetired() {
+        return dateRetired;
+    }
+
+    public void setDateRetired(LocalDateTime dateRetired) {
+        this.dateRetired = dateRetired;
+    }
+
+    public String getRetiredReason() {
+        return retiredReason;
+    }
+
+    public void setRetiredReason(String retiredReason) {
+        this.retiredReason = retiredReason;
+    }
+
+    public Integer getConfidential() {
+        return confidential;
+    }
+
+    public void setConfidential(Integer confidential) {
+        this.confidential = confidential;
+    }
 }
--- core/src/main/java/zm/gov/moh/core/repository/database/entity/domain/AppointmentschedulingBlockTypeMap.java
+++ core/src/main/java/zm/gov/moh/core/repository/database/entity/domain/AppointmentschedulingBlockTypeMap.java
@@ -1,10 +1,34 @@
 package zm.gov.moh.core.repository.database.entity.domain;
 
-import androidx.room.Entity;
+import androidx.room.*;
+
+import com.squareup.moshi.Json;
 
 @Entity(tableName = "appointmentscheduling_block_type_map")
 public class AppointmentschedulingBlockTypeMap {
 
-    public Long appointment_type_id;
-    public Long appointment_block_id;
+    @PrimaryKey
+    @ColumnInfo(name = "appointment_type_id")
+    @Json(name = "appointment_type_id")
+    private Long appointmentTypeId;
+
+    @ColumnInfo(name = "appointment_block_id")
+    @Json(name = "appointment_block_id")
+    private Long appointmentBlockId;
+
+    public Long getAppointmentTypeId() {
+        return appointmentTypeId;
+    }
+
+    public void setAppointmentTypeId(Long appointmentTypeId) {
+        this.appointmentTypeId = appointmentTypeId;
+    }
+
+    public Long getAppointmentBlockId() {
+        return appointmentBlockId;
+    }
+
+    public void setAppointmentBlockId(Long appointmentBlockId) {
+        this.appointmentBlockId = appointmentBlockId;
+    }
 }
