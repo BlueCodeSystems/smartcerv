f88991281c98fea6417c64cd9964f153e9dbe68c
Kaunda Kabwe - Work PC <kkabwe@bluecodeltd.com>
2020-01-15 15:58:06 +0200

    client card on dashboard temporary fix.

--- core/src/main/java/zm/gov/moh/core/repository/database/dao/derived/GenericDao.java
+++ core/src/main/java/zm/gov/moh/core/repository/database/dao/derived/GenericDao.java
@@ -26,7 +26,7 @@ public  interface GenericDao {
     @Query("SELECT patient.patient_id,patient_identifier.identifier, given_name, family_name, gender, birthdate FROM person JOIN person_name ON person.person_id = person_name.person_id JOIN patient ON patient.patient_id = person.person_id JOIN patient_identifier ON patient.patient_id = patient_identifier.patient_id WHERE person_name.preferred = 1 AND patient.voided = 0 AND patient_identifier.voided !=1 AND identifier_type = 4 AND patient.patient_id = :id")
     LiveData<Client> getPatientById(long id);
 
-    @Query("SELECT patient.patient_id,patient_identifier.identifier, given_name, family_name, gender, birthdate FROM person JOIN person_name ON person.person_id = person_name.person_id JOIN patient ON patient.patient_id = person.person_id JOIN patient_identifier ON patient.patient_id = patient_identifier.patient_id WHERE person_name.preferred = 1 AND patient.voided = 0 AND patient_identifier.voided !=1 AND identifier_type = 6 AND patient.patient_id = :id")
+    @Query("SELECT patient.patient_id,patient_identifier.identifier, given_name, family_name, gender, birthdate FROM person JOIN person_name ON person.person_id = person_name.person_id JOIN patient ON patient.patient_id = person.person_id JOIN patient_identifier ON patient.patient_id = patient_identifier.patient_id WHERE person_name.preferred = 1 AND patient.voided = 0 AND patient_identifier.voided !=1 AND identifier_type = 7 AND patient.patient_id = :id")
     LiveData<Client> getMdrPatientById(long id);
 
     //get getPersons by id
