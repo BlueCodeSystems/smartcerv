12c34f53fe32bcbee6420dc2f0a1d192721e9673
csamiselo <csamiselo@github.com>
2020-01-21 16:45:57 +0200

    progress so far

---
+++ drugresistanttb/src/main/java/zm/gov/moh/drugresistanttb/submodule/dashboard/patient/view/BacteriologicalExaminationActivity.java
@@ -0,0 +1,15 @@
+package zm.gov.moh.drugresistanttb.submodule.dashboard.patient.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import zm.gov.moh.drugresistanttb.R;
+
+public class BacteriologicalExaminationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bacteriological_exam);
+    }
+}
